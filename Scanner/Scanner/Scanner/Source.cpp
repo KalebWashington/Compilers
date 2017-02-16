5850b4ce33ad1d5cbbdec66e8bcc99908f143bbd
Alan <alansottthomas@gmail.com>
2017-02-15 22:11:08 -0600

    Undoing test commit. -AT

--- Scanner/Scanner/Scanner/Source.cpp
+++ Scanner/Scanner/Scanner/Source.cpp
@@ -1,5 +1,3 @@
-//testing commit on laptop
-
 /*
 Initial notes:
 If we decide to read in entire words at a time we need to change chars to strings in the load code function, along with the initial vector build in the main function, in addition to changing char word to string word.
