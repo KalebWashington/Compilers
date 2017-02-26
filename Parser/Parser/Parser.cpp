a62ad8cb0d6bc60e2d2124f670a591bf92f04309
Alan <Alan@ALAN-SP>
2017-02-26 13:27:42 -0600

    Configuring files

--- Parser/Parser/Parser.cpp
+++ Parser/Parser/Parser.cpp
@@ -1,5 +1,13 @@
-/*
-Starting parser
-test
-test
-*/
+#include "Scanner.h" //including Scanner functions
+#include "SymbolTable.h" //including SymbolTable functions
+
+
+using namespace std;
+
+
+
+int main()
+{
+
+	return 0;
+}
