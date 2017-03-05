d078e2cabdb2330ac7c6fc404b6df10e03c1b02a
jprobillard <jprobillard@CLARK-Z103-FA16>
2017-03-05 14:59:14 -0600

    Updated Parser

--- CompilerTests/CompilerTests/parser.h
+++ CompilerTests/CompilerTests/parser.h
@@ -10,7 +10,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "Scanner.h"
+#include "scanner.h"
 #include "Symboltable.h"
 
 using namespace std;
@@ -18,25 +18,46 @@ using namespace std;
 class Parser
 {
 private:
+		
 	vector <char> parsInCode;
-	
+	Scanner scannerobj;
+
 public:
+
+	struct TOKENS                                                                         
+	{
+		string token_name;                                                                
+	};
+
 	Parser::Parser(vector <char> code) 
 	{
 		cout << "this is size of code from parsConstruct: " << code.size() << endl;
+		cout << "this is size of code from parsConstruct: " << parsInCode.size() << endl;
+
 		parsInCode = code;
+		
+
+		cout << "this is size of code from parsConstruct: " << code.size() << endl;
+		cout << "this is size of parsInCode from parsConstruct: " << parsInCode.size() << endl;
+
 	}
 
 void Parser::testOutput()
 	{
-	
-		cout << "this is size of code from parsTest: " << parsInCode.size() << endl;
+		cout << "this is size of parsInCode from parsTest: " << parsInCode.size() << endl;
 
-		/*
 		ofstream textout("output2.txt", ios::out);
 		textout.write((const char*)&parsInCode[0], parsInCode.size()-1);
 		textout.close();
-		*/
+	}
+
+void Parser::scannerRun()
+	{
+		vector <Scanner::TOKENS> tokens_f_parser;
+
+		scannerobj.build_tokens(parsInCode,tokens_f_parser);
+		
+
 	}
 };
 
