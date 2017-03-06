6fd6517008464d0148890fef506251f4bc6c4666
JeremyVasseur <kornmr2@yahoo.com>
2017-03-05 18:01:26 -0600

    Newest From Jebbzilla

---
+++ NewFull/NewFull/Parser.cpp
@@ -0,0 +1,88 @@
+#ifndef PARSER_H_INCLUDED
+#define PARSER_H_INCLUDED
+
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "scanner.h"
+#include "Symboltable.h"
+
+using namespace std;
+
+class Parser
+{
+private:
+
+	vector <char> parsInCode;
+	Scanner scannerobj;
+
+public:
+
+	struct TOKENS
+	{
+		string token_name;
+	};
+
+	Parser::Parser(vector <char> code)
+	{
+		cout << "this is size of code from parsConstruct: " << code.size() << endl;
+		cout << "this is size of code from parsConstruct: " << parsInCode.size() << endl;
+
+		parsInCode = code;
+
+		cout << "this is size of code from parsConstruct: " << code.size() << endl;
+		cout << "this is size of parsInCode from parsConstruct: " << parsInCode.size() << endl;
+
+	}
+
+	void Parser::testOutput()
+	{
+		cout << "this is size of parsInCode from parsTest: " << parsInCode.size() << endl;
+
+		ofstream textout("output2.txt", ios::out);
+		textout.write((const char*)&parsInCode[0], parsInCode.size() - 1);
+		textout.close();
+	}
+
+	/*
+	vector <char> Parser::scannerCharVectorFill()
+	{
+	return parsInCode;
+	}
+	*/
+
+	void Parser::scannerRun()
+	{
+		vector <Scanner::TOKENS> tokens_f_parser;
+
+		scannerobj.grabCharVector(parsInCode);
+
+		tokens_f_parser = (scannerobj.build_tokens(tokens_f_parser));
+
+		while (tokens_f_parser.at(0).token_name != "DoneToken")
+		{
+			if (tokens_f_parser.at(0).token_name == "IntDecToken")
+			{
+				cout << "Success! IntDecToken If Statement from Parser\n";
+				//call match function
+			}
+			//tokens_f_parser.pop_back();
+			tokens_f_parser = (scannerobj.Scanner::build_tokens(tokens_f_parser));
+		}
+
+		cout << "Success! DoneToken from Parser\n";
+
+	}
+};
+
+/*
+
+*/
+
+#endif
---
+++ NewFull/NewFull/Scanner.h
@@ -0,0 +1,51 @@
+#ifndef Scanner_H_INCLUDED
+#define Scanner_H_INCLUDED
+
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+class Scanner
+{
+private:
+
+public:
+	struct TOKENS                                                                         //TOKENS struct; used to create a vector of this type that will hold the tokens generated.
+	{
+		string token_name;                                                                //string to hold the name of the generated token
+	};
+
+	vector<char> code;
+
+	//PROTOTYPES//
+	void print_tokens(vector<TOKENS>& tokens);                                             //function to print tokens generated. this will more than likely be removed as it was used for testing.
+
+																						   //	void elements_delete(vector<char>code, vector<TOKENS>& tokens, int counter);           //function to delete the number of character from the code vector that was used to generate the token
+
+	vector <Scanner::TOKENS> build_tokens(vector<TOKENS>& tokens);                          //function that will iterate through the code vector looking for tokens
+
+	void load_code(vector<char> code, vector<TOKENS>& tokens);                            //function to read from the 'code.txt' file which holds the tokens to pass to the scanner
+
+	void insert_symbol_table(vector<TOKENS>& tokens);                                      //insert into symbol table function
+
+																						   //void grabCharVector(vector<char> code);
+
+	void initial();                                                                       //function that will call the build function, may be removed in the final compiler build
+																						  //----------------------------------------//
+	void grabCharVector(vector<char> code_from_parser)
+	{
+		code = code_from_parser;
+		cout << "size of code in scanner: " << Scanner::code.size() << endl;
+	}
+
+
+};
+#endif#pragma once
